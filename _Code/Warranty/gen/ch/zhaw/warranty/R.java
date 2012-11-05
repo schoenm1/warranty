a67beddbbcc6c2b0c7e6d6d330cb7e60784aefc3
Andreas Gruenenfelder <gruenean@students.zhaw.ch>
2012-11-06 00:21:19 +0100

    project working again..

--- _Code/Warranty/gen/ch/zhaw/warranty/R.java
+++ _Code/Warranty/gen/ch/zhaw/warranty/R.java
@@ -14,8 +14,7 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int icon=0x7f020000;
     }
     public static final class id {
         public static final int BTDeleteAll=0x7f070010;
