00ab121a64eeea1ac11bf830ebe6512eb9af9d5a
Andreas Gruenenfelder <gruenean@students.zhaw.ch>
2012-11-13 23:58:29 +0100

    added toString() method to customize the displayed values in listadapter

--- _Code/Warranty/src/ch/zhaw/warranty/card/WarrantyCard.java
+++ _Code/Warranty/src/ch/zhaw/warranty/card/WarrantyCard.java
@@ -52,4 +52,15 @@ public class WarrantyCard {
 	public String getReseller() {
 		return reseller;
 	}
+	
+	public String toString() {
+		System.out.println("title: " + title);
+		System.out.println("desc: " + description);
+		System.out.println("imgp: " + imagePath);
+		System.out.println("create: " + createdAt);
+		System.out.println("vld: " + validUntil);
+		System.out.println("pric: " + price);
+		System.out.println("rese: " + reseller);
+		return title;
+	}
 }
